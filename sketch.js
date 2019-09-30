5034f27cf96576c67a4c0e5718943f27872817a5
wkuglen <wkuglen@gmail.com>
2019-09-30 15:25:05 -0500

    Cleanup code

--- sketch.js
+++ sketch.js
@@ -35,22 +35,9 @@ function setup() {
   rectWidth = width / 4;
   index = 0;
   imgSelect = 0;
-  // columnA_key = 'a'.charCodeAt(0);
-  // columnA = width / 4;
-  // columnS_key = 's'.charCodeAt(0);
-  // columnS = width / 2;
-  // columnD_key = 'd'.charCodeAt(0);
-  // columnD = 3*width / 4;
-  // currentColumn = 0;//columnA;
   img = loadImage('assets/01.jpg');
   imgRecyclable = false;
-  // img2 = loadImage('assets/02.jpg');
-  // img3 = loadImage('assets/03.jpg');
-  // images = [img1, img2, img3];
-  // columns = [width/4, width/2, 3*width/4]
-  // fr = 5;
-  // frameRate(fr);
-  // img = 
+
   lives = 3;
   numCorrect = 0;
   numGuessed = 0;
@@ -111,34 +98,13 @@ function draw() {
   } else {
     // fill(0,0,0)
   }
-  // ellipse(imageWidth+80,0, 80, 80);
-  // index=index+stepSize;
-  // if (index > 600) {
-  //   index = 0;
-  //   imgSelect++;
-  //   if (imgSelect > 2 ){
-  //     imgSelect = 0;
-  //   }
-  //   // fr = fr+1;
-  //   // if(fr>60) {
-  //   //   fr = 60;
-  //   // }
-  //   stepSize++;
-  //   // frameRate(fr);
-  // }
-  // line(topLeftW, topLeftH, imageWidth+topLeftW, topLeftH);
-  image(img, topLeftW, topLeftH, imageWidth, imageHeight);
-  // image(images[imgSelect], columns[currentColumn]-51, index,102,76);
-  // image(img2, columnS-51, index,102,76);
-  // image(img3, columnD-51, index-100,102,76);
   
-
+  // line(topLeftW, topLeftH, imageWidth+topLeftW, topLeftH);
+  image(img, topLeftW, topLeftH, imageWidth, imageHeight);  
 }
 
 function keyPressed() {
   let keyIndex = -1;
-  // let pressed = key.charCodeAt(0);
-  // console.log(key.charCodeAt(0));
   guessRecyclable = false;
   guessLock = false;
   if (key == 'r') {
@@ -163,19 +129,6 @@ function keyPressed() {
       getImage();
     }
   }
-  // if (keyIndex === -1) {
-  //   // If it's not a letter key, clear the screen
-  //   background(230);
-  //   getImage();
-  // } else {
-  //   // It's a letter key, fill a rectangle
-  //   randFill_r = Math.floor(Math.random() * 255 + 1);
-  //   randFill_g = Math.floor(Math.random() * 255 + 1);
-  //   randFill_b = Math.floor(Math.random() * 255 + 1);
-  //   fill(randFill_r, randFill_g, randFill_b);
-  //   let x = map(keyIndex, 0, 25, 0, width - rectWidth);
-  //   // rect(x, 0, rectWidth, height);
-  // }
 }
 
 function timeDiff(h1,m1,s1, h2,m2,s2) {
